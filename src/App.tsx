7dc7d266ffa4896eac23d72daebc0b4deb461bfb
Anderson <andersonlosada.11@gmail.com>
2025-11-25 21:36:00 -0500

    main app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 // import { BasicTypes } from './typescript/BasicTypes';
-
-import './App.css';
+// import { ObjectLiterals } from './typescript/ObjectLiterals';
+// import { BasicFunctions } from './typescript/BasicFunctions';
+// import { Counter } from './components/Counter';
+import { LoginPage } from './components/LoginPage';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <>
-      <h1>React + TS</h1>
-      {/* <BasicTypes /> */}
-    </>
+    <AuthProvider>
+      <div className='flex flex-col justify-center items-center h-svh'>
+        <h1 className='text-4xl mb-5'>React + TS</h1>
+        {/* <BasicTypes /> */}
+        {/* <ObjectLiterals /> */}
+        {/* <BasicFunctions /> */}
+        {/* <Counter /> */}
+        <LoginPage />
+      </div>
+    </AuthProvider>
   );
 }
 
