0068ea58d7ef57727b7365caece1a04b4ce01c58
Anderson <andersonlosada.11@gmail.com>
2025-11-25 22:38:19 -0500

    LoginPage authentication

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,16 +1,44 @@
 import { useAuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
-  const { isChecking } = useAuthContext();
-  
+  const {
+    isChecking,
+    isAuthenticated,
+    loginWithEmailAndPassword,
+    logout,
+    user,
+  } = useAuthContext();
+
   if (isChecking) {
     return <h3>Verificando usuario</h3>;
   }
 
   return (
     <>
-      <h3>Login</h3>
-      <span></span>
+      {isAuthenticated ? (
+        <>
+          <h3>Bienvenido</h3>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+          <button
+            onClick={() => logout()}
+            className='bg-blue-500 p-2 text-white rounded-xl mt-2'
+          >
+            Salir
+          </button>
+        </>
+      ) : (
+        <>
+          <h3>Ingresar a la aplicación</h3>
+          <button
+            onClick={() =>
+              loginWithEmailAndPassword('obi@google.com', '123456')
+            }
+            className='bg-blue-500 p-2 text-white rounded-xl mt-2'
+          >
+            Ingresar
+          </button>
+        </>
+      )}
     </>
   );
 };
